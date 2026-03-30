2e8a0ca84a0f16871d6465b867df11c75b41a670
Aralbekov Umar <umarmirlanbek@gmail.com>
2026-03-30 18:10:10 +0600

    i fixed some details

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'airBNB_app29',
+        'USER': 'postgres',
+        'PASSWORD': 'topor231',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
